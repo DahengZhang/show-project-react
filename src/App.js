e632fc644a541be5b54be89f10963031e2c2f9aa
DahengZhang <Daheng_Zhang@outlook.com>
2020-01-29 12:32:57 +0800

    项目初始化

--- src/App.js
+++ src/App.js
@@ -1,34 +1,14 @@
 import React, { Component, Fragment } from 'react'
-import { connect } from 'react-redux'
 
 class App extends Component {
     render () {
         return (
             <Fragment>
-                <input
-                    value={this.props.inputValue}
-                    onChange={this.props.changeInputValue} />
+                <h1>Hello World</h1>
             </Fragment>
         )
     }
 
 }
 
-const mapStateProps = state => {
-    return {
-        inputValue: state.inputValue
-    }
-}
-
-const mapDispatchToProps = dispatch => {
-    return {
-        changeInputValue (e) {
-            dispatch({
-                type: 'change_input_value',
-                value: e.target.value
-            })
-        }
-    }
-}
-
-export default connect(mapStateProps, mapDispatchToProps)(App)
+export default App
